4138ed8daf5c2da43ef77bcc428d820bb5ec998e
Zhenhua Dong <zhenhua.dong@ericsson.com>
2018-03-22 13:44:54 +0800

    SC : add exception handle for out of memory

--- zhenhua.homelake/src/main/java/com/zhenhua/homelake/MemoryDemo.java
+++ zhenhua.homelake/src/main/java/com/zhenhua/homelake/MemoryDemo.java
@@ -17,20 +17,31 @@ public class MemoryDemo
     	if(i%10 == 0){
     		System.out.println(i);
     	}
-    	stackOverflow(++i);
+//    	try {
+    		stackOverflow(++i);
+//    	}catch (StackOverflowError e) {
+//    		System.out.println(i);
+////    		e.printStackTrace();
+//    		throw new RuntimeException();
+//    	}
     }
     
     public static void heapOutOfMemory(int i){
-    	
     	StringBuffer sb = new StringBuffer();
     	for(int j=0; j<1024*1024; j++){
     		sb.append("a");
     	}
     	
-    	if(i%10 == 0){
+//    	if(i%10 == 0){
+//    		System.out.println(i);
+//    	}
+    	try {
+    		heapOutOfMemory(++i);
+    	} catch (OutOfMemoryError e) {
     		System.out.println(i);
+    		e.printStackTrace();
+    		throw new RuntimeException();
     	}
-    	heapOutOfMemory(++i);
     	System.out.println(sb.toString());
     }
     
