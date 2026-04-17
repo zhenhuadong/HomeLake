423c0517cd325382eba48e53c1c310bc0dd9ed83
Dong Zhenhua <dongzhenhua@gmail.com>
2026-04-17 16:43:34 +0800

    openai responses api and completions api

--- zhenhua.ai/openai/demo_aliyun.py
+++ zhenhua.ai/openai/demo_aliyun.py
@@ -3,17 +3,25 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-def get_response():
-    client = OpenAI(
-        api_key=os.getenv("DASHSCOPE_API_KEY"),
-        base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
-    )
-    completion = client.chat.completions.create(
-        model="qwen-plus",  # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
-        messages=[{'role': 'system', 'content': 'You are a helpful assistant.'},
-                  {'role': 'user', 'content': '你是谁？'}]
-        )
-    print(completion.model_dump_json())
+client = OpenAI(
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
 
-if __name__ == '__main__':
-    get_response()
+# # response API 最新的的接口，适用于所有模型，推荐使用 
+# response = client.responses.create(
+#   model="qwen-plus",
+#   input="Explain how AI works in a few words",
+#   store=True,
+# )
+
+# print(response.output_text);
+
+# completion API 适用于部分模型，后续会逐步废弃，不推荐使用
+completion = client.chat.completions.create(
+    model="qwen-plus",  # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+    messages=[{'role': 'system', 'content': 'You are a helpful assistant.'},
+                {'role': 'user', 'content': 'Explain how AI works in a few words'}]
+    )
+# print(completion.model_dump_json())
+print(completion.choices[0].message.content)
--- zhenhua.ai/openai/demo_openai.py
+++ zhenhua.ai/openai/demo_openai.py
@@ -4,10 +4,20 @@ load_dotenv()
 
 client = OpenAI()
 
+# response API 最新的的接口，适用于所有模型，推荐使用
 response = client.responses.create(
   model="gpt-5-nano",
-  input="write a haiku about ai",
+  input="Explain how AI works in a few words",
   store=True,
 )
 
 print(response.output_text);
+
+# # completion API 适用于部分模型，后续会逐步废弃，不推荐使用
+# completion = client.chat.completions.create(
+#     model="gpt-5-nano", 
+#     messages=[{'role': 'system', 'content': 'You are a helpful assistant.'},
+#                 {'role': 'user', 'content': 'Explain how AI works in a few words'}]
+#     )
+
+# print(completion.choices[0].message.content)
